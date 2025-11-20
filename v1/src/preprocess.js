a750aeddfb874a93365b5dcd661db76b2a88c551
123456789hien <66470873+123456789hien@users.noreply.github.com>
2025-11-20 16:44:15 +0300

    Delete v1 directory

--- v1/src/preprocess.js
+++
@@ -1,5 +0,0 @@
-export async function parseCSVFile(file){
-  return new Promise((resolve,reject)=>{
-    Papa.parse(file,{header:true,skipEmptyLines:true,complete:(res)=>resolve(res.data),error:err=>reject(err)});
-  });
-}
